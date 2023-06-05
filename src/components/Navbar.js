3221ccf8deb38accab8c1a6173c4eacb049dad14
Lakshay Kumar Kashyap <131148878+LakshayKumar9347@users.noreply.github.com>
2023-06-05 08:21:44 +0530

    update navbar.js
    added bootstrap modals

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,6 +14,16 @@ export default function Navbar(props) {
     color: `${Lcolor}`,
 
   }
+  // Get the color picker element
+  var colorPicker = document.getElementById("colorPicker");
+
+  // Function to save the selected color
+  function saveColor() {
+    var selectedColor = colorPicker.value;
+    // You can store the selectedColor value in a variable or send it to a server using AJAX.
+    console.log("Selected Color:", selectedColor);
+  }
+
   return (
     <nav className={`navbar navbar-expand-lg  navbar-${props.prop} bg-${props.prop}`}>
       <div className="container-fluid">
@@ -30,6 +40,33 @@ export default function Navbar(props) {
               <Link className="nav-link" to='/about' >{props.about}</Link>
             </li>
           </ul>
+          {/* //! warning  */}
+          {/* <!-- Button trigger modal --> */}
+          <button type="button" className="btn btn-primary m-3" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Custom Color
+          </button>
+
+          {/* <!-- Modal --> */}
+          <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h1 className="modal-title fs-5 text-dark" id="exampleModalLabel">Modal title</h1>
+                  <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div className="modal-body text-dark">
+                  <input type="color" id="colorPicker" value="#808080" />
+
+                </div>
+                <div className="modal-footer">
+                  <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="button" className="btn btn-primary" onClick={saveColor}>Save changes</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* //! warning  */}
+
           <div className="form-check form-switch">
             <input className="form-check-input" onClick={props.mode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
             <label className="form-check-label" htmlFor="flexSwitchCheckDefault " style={labelStyle}>{props.prop === 'light' ? "Enable Dark Mode" : "Disable Dark Mode"}</label>
